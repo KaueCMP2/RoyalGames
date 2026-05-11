f83536a5e54f71a2c43f9c8fa51bc423f96e6bdf
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-11 10:59:16 -0300

    :sparkles: feat: add complete API

---
+++ API/RoyalGames/DTOs/GeneroDto/CriarGeneroDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RoyalGames.DTOs.GeneroDto
+{
+    public class CriarGeneroDto
+    {
+        //Inicializado como null para evitar warning
+        public string Nome { get; set; } = null!;
+    }
+}
---
+++ API/RoyalGames/DTOs/PlataformaDto/CriarPlataformaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RoyalGames.DTOs.PlataformaDto
+{
+    public class CriarPlataformaDto
+    {
+        // Inicializado com null! para evitar warning do compilador
+        public string Nome { get; set; } = null!;
+    }
+}
