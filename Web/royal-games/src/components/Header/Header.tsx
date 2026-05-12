326d961fc32f2b2c351d5cbda9b60084b89aaca8
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-12 08:39:35 -0300

    :sparkles: feat: complete component header

--- Web/royal-games/src/components/Header/Header.tsx
+++ Web/royal-games/src/components/Header/Header.tsx
@@ -4,10 +4,12 @@ import styles from '@/components/Header/Header.module.css'
 const Header = () => {
     return (
         <header id={styles.header}>
-            <img src="" alt="" />
-            <div id={styles.icons_right}>
-                <a href="#jogos">Jogos</a>
-                <a href="#perfil"></a>
+            <div id={styles.content_header} className='layout-grid'>
+                <img src="../logo.png" alt="Logo Royal games em vermelho" id={styles.logo} />
+                <div id={styles.icons_right}>
+                    <a href="#jogos">Jogos</a>
+                    <a href="#perfil" id={styles.link_perfil}>Perfil</a>
+                </div>
             </div>
         </header>
     )
--- Web/royal-games/src/pages/home/index.tsx
+++ Web/royal-games/src/pages/home/index.tsx
@@ -1,8 +1,11 @@
+import Header from '@/components/Header/Header'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Header />
+    </>
   )
 }
 
