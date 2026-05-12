d3f457eb62d2cfe3aa5b3bd8d60e16aa5ed1f839
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-12 10:54:59 -0300

    :sparkles: feat: creating cardGame component

--- Web/royal-games/src/components/CardGame/CardGame.tsx
+++ Web/royal-games/src/components/CardGame/CardGame.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from '@/components/CardGame/CardGame.module.css'
 
 export const CardGame = () => {
-  return (
-    <div>CardGame</div>
-  )
+    return (
+        <li id={styles.card}>
+            <div id={styles.card_content}>
+                <img src="../game-cover/minecraft_legends.pnh" alt="Capa do jogo minecraft legends" id={styles.game_cover} />
+                <h3>Minecraft</h3>
+                <p>R$70,00</p>
+                <button>Detalhes</button>
+            </div>
+        </li>
+    )
 }
