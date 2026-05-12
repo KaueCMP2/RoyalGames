be87d2eda0af24a19a355261e295e0fde500a29b
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-12 10:54:45 -0300

    :sparkles: feat: creating gameList component

--- Web/royal-games/src/components/GameList/GameList.tsx
+++ Web/royal-games/src/components/GameList/GameList.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from '@/components/GameList/GameList.module.css'
 
 const GameList = () => {
-  return (
-    <div>GameList</div>
-  )
+    return (
+        <section id={styles.game_list}>
+            <h2>Catálogo de jogos</h2>
+
+            <div id={styles.container_search}>
+                <input type="text" placeholder='pesquise...' id={styles.search_input} />
+
+                <div id={styles.container_buttons}>
+                    <button id={styles.btn_price}>Menor Preço</button>
+                    <button id={styles.btn_category}>Categoria</button>
+                </div>
+            </div>
+
+            <ul id={styles.container_games}>
+
+            </ul>
+        </section>
+    )
 }
 
 export default GameList
