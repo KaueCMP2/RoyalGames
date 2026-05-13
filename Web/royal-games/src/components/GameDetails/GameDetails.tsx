82b468d8e6cb9fc9009793ad0cd46cf411e2579f
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-13 10:59:37 -0300

    :sparkles: feat: initial component gameDetails structure

---
+++ Web/royal-games/src/components/GameDetails/GameDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@/components/GameDetails/GameDeatils.module.css'
+
+const GameDetails = () => {
+  return (
+    <></>
+    // <section id={styles.container_game_details}>
+
+    // </section>
+  )
+}
+
+export default GameDetails
