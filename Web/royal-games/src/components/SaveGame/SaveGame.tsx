7a1b49d0ae789aaa1f04e536f4dda09711db5d1b
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-21 22:07:28 -0300

    :plus: add logic && api consume on save game component

--- Web/royal-games/src/components/SaveGame/SaveGame.tsx
+++ Web/royal-games/src/components/SaveGame/SaveGame.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/components/SaveGame/SaveGame.module.css'
+import { Alex_Brush } from 'next/font/google'
+import { cadastrarJogos } from '@/pages/api/jogoService'
+import { api } from '@/pages/api/apiService'
+import { listarGeneros } from '@/pages/api/generosService'
+import { listarPlataformas } from '@/pages/api/plataformasService'
+import { listarClassificacao } from '@/pages/api/classificacaoService'
+
+interface Classificacao {
+    ClassificacaoId: number;
+    Nome: string;
+}
+
+interface Genero {
+    GeneroId: number,
+    Nome: string
+}
+
+interface Plataforma {
+    PlataformaId: number,
+    Nome: string
+}
 
 const SaveGame = () => {
+    const [classificacoes, setClassificacoes] = useState<Classificacao[]>([])
+    const [generos, setGeneros] = useState<Genero[]>([])
+    const [plataformas, setPlataformas] = useState<Plataforma[]>([])
+
+    const [nome, setNome] = useState<string>("")
+    const [valor, setValor] = useState<string>("")
+    const [descricao, setDescricao] = useState<string>("")
+    const [imagem, setImagem] = useState<File | null>(null)
+    const [classificacaoSelecionada, setClassificacaoSeleciona] = useState<number[]>([])
+    const [generosSelecionados, setGenerosSelecionados] = useState<number[]>([])
+    const [plataformasSelecionadas, setPlataformasSelecionadas] = useState<number[]>([])
+
+    async function listarGeneroSel() {
+        const lista = await listarGeneros();
+        setGeneros(lista);
+    }
+
+    async function listarPlataformasSel() {
+        const lista = await listarPlataformas()
+        setPlataformas(lista);
+    }
+
+    async function listarClassificacaoSel() {
+        const lista = await listarClassificacao()
+        setClassificacoes(lista);
+    }
+
+    const jogo = {
+        nome,
+        valor,
+        descricao,
+        imagem,
+        generoIds: generosSelecionados,
+        classificacaoIndicativaId: classificacaoSelecionada,
+        plataformaIds: plataformasSelecionadas,
+    }
+
+    useEffect(() => {
+        listarGeneroSel();
+        listarPlataformasSel();
+        listarClassificacaoSel();
+    }, [])
+
     return (
         <section id={styles.save_game}>
             <form action="" id={styles.form_save_game} className='layout-grid'>
@@ -14,36 +78,71 @@ const SaveGame = () => {
                         <div id={styles.row1}>
                             <div id={styles.inpt1}>
                                 <label htmlFor="game-name">Nome</label>
-                                <input type="text" name='game-name' />
+                                <input type="text" name='game-name' onChange={(e) => setNome(e.target.value)} />
                             </div>
                         </div>
 
                         <div id={styles.row2}>
                             <div id={styles.inpt2}>
                                 <label htmlFor="game-price">valor</label>
-                                <input type="text" name='game-price' id={styles.inpt_price} />
+                                <input type="text" name='game-price' id={styles.inpt_price} onChange={(e) => setValor(e.target.value)} />
                             </div>
 
                             <div id={styles.inpt3}>
                                 <label htmlFor="game-genre">Gênero</label>
-                                <input type="text" name='game-genere' id={styles.inpt_genre} />
+                                <select name='game-genere' className={styles.select}
+                                    multiple
+                                    value={generosSelecionados.map(String)}
+                                    onChange={(e) => setGenerosSelecionados(
+                                        Array.from(e.target.selectedOptions).map((option) => Number(option.value))
+                                    )}>
+                                    {generos.map((item) => (
+                                        <option value={item.GeneroId}
+                                            key={item.GeneroId}>{item.Nome}</option>
+                                    ))}
+                                </select>
                             </div>
 
                             <div id={styles.inpt4}>
                                 <label htmlFor="game-indicative-class">Classificação indicativa</label>
-                                <input type="text" name='game-indicative-class' id={styles.inpt_class} />
+                                <select name='game-indicative-class' className={styles.select}
+                                    multiple
+                                    value={classificacaoSelecionada.map(String)}
+                                    onChange={(e) => setClassificacaoSeleciona(
+                                        Array.from(e.target.selectedOptions).map((option) => Number(option.value))
+                                    )}>
+                                    {classificacoes.map((item) => (
+                                        <option
+                                            key={item.ClassificacaoId}
+                                            value={item.ClassificacaoId}>{item.Nome}</option>
+                                    ))}
+                                </select>
                             </div>
                         </div>
 
                         <div id={styles.row3}>
                             <div id={styles.inpt5}>
                                 <label htmlFor="game-platform">Plataforma</label>
-                                <input type="text" name='game-platform' id={styles.inpt_plat} />
+                                <select name='game-platform' className={styles.select}
+                                    multiple
+                                    value={plataformasSelecionadas.map(String)}
+                                    onChange={(e) => setPlataformasSelecionadas(
+                                        Array.from(e.target.selectedOptions).map((option) => Number(option.value))
+                                    )}>
+                                    {plataformas.map((item) => (
+                                        <option value={item.PlataformaId}
+                                            key={item.PlataformaId}>{item.Nome}</option>
+                                    ))}
+                                </select>
                             </div>
 
                             <div id={styles.inpt6}>
                                 <label htmlFor="game-image">Imagem</label>
-                                <input type="text" name='game-image' id={styles.inpt_img} />
+                                <input type="file" name='game-image' id={styles.inpt_img} onChange={(e) => {
+                                    if (e.target.value && e.target.files![0]) {
+                                        setImagem(e.target.files![0])
+                                    }
+                                }} />
                             </div>
                         </div>
                     </div>
@@ -51,14 +150,14 @@ const SaveGame = () => {
                     <div id={styles.side2}>
                         <div id={styles.inpt7}>
                             <label htmlFor="game-description">Descrção</label>
-                            <textarea name='game-image' id={styles.textarea}></textarea>
+                            <textarea name='game-description' id={styles.textarea} onChange={(e) => setDescricao(e.target.value)}></textarea>
                         </div>
                     </div>
                 </div>
 
                 <button>Cadastrar</button>
             </form>
-        </section>
+        </section >
     )
 }
 
